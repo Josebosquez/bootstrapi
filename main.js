32979a2a805c0d129ba8748efc2cc1c3f202f0db
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-04-11 22:18:47 -0600

    addition of rick and morty api with adjustments to dog generator

--- main.js
+++ main.js
@@ -31,7 +31,8 @@ button.addEventListener("click", function (updateImg){
 
 // query
 const wButton = document.querySelector('.btn1');
-const textInput = document.querySelector("[type = 'text']")  ;
+const textInput = document.querySelector("[type = 'text']")  
+const ImageSrc = document.querySelector("#weather")
 const temp = document.querySelector("#Temp");
 const wind = document.querySelector("#wind");
 const DD = document.querySelector("#DD");
@@ -57,5 +58,41 @@ wButton.addEventListener("click", function(){
         temp.innerHTML = "Temperature: " + city.temperature
         wind.innerHTML = "Wind: " + city.wind
         DD.innerHTML = "Data Description: " + city.description
+        if (city.temperature < 20){
+            ImageSrc = "/images/sunny.jpeg"
+            console.log(ImageSrc)
+        }
     })    
 })
+
+//================
+//Rick and Morty generator
+//================
+
+//queries 
+const buttonRM = document.querySelector('.btn2')
+const photo = document.querySelector("#photo")
+const FF1 = document.querySelector("#fun1")
+const FF2 = document.querySelector("#fun2")
+const FF3 = document.querySelector("#fun3")
+
+//update img add event listener
+buttonRM.addEventListener("click", function (updateImg){
+    //step 1: send a get req to the api url
+    const URL =  "https://rickandmortyapi.com/api/character"
+    fetch(URL)
+    
+    //step2: extract JSOm from the URL RESponse
+    .then(function(rawResponse) {
+        console.log("response:", rawResponse)
+        return rawResponse.json()
+    })
+    //Step 3: Process JSON to replace img src with img message.
+    .then (function (updateImg){
+        photo.src = updateImg.results[4].image;
+        console.log(updateImg.results[4].image);
+        FF1.innerHTML = "Name of Character: " + updateImg.results[4].name
+        FF2.innerHTML = "Character's species: " + updateImg.results[4].species
+        FF3.innerHTML = "Character's origin: " + updateImg.results[4].origin.name
+    })    
+}) 
